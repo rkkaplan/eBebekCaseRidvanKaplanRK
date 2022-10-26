60cb0e2d8ba2a28da0bf9e89fd0d42c3d4bd78ec
ridvan kaplan <rkkaplanhny@gmail.com>
2022-10-26 23:24:39 +0300

    Local Date Time

--- src/Employee2.java
+++ src/Employee2.java
@@ -1,5 +1,15 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Employee2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        LocalDateTime suAnkiTarihSaat = LocalDateTime.now();
+        System.out.println(suAnkiTarihSaat);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println(dtf.format(suAnkiTarihSaat));
+
+
     }
 }
